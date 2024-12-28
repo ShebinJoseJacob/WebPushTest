29ff033d07ec700b51400efdbd9c816a36bffb51
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2024-12-28 11:49:20 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const webPush = require('web-push');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
